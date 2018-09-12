963bfe5d987a8c315cee2baf263d2acad236a874
Art Ser <artser97@yandex.ru>
2018-09-12 16:59:52 +0300

    art

--- src/index.js
+++ src/index.js
@@ -4,4 +4,21 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let controlArr = [];
+let count = 0;
+for(let i = 0; i<preferences.length; i++){
+    let index1 = preferences[i] - 1;
+    if( index1 === i) continue;
+    let index2 = preferences[index1] - 1;
+    let index3 = preferences[index2] - 1;
+    if (index3 === i){
+        if (controlArr.indexOf(index1) === -1 && controlArr.indexOf(index2) === -1 &&controlArr.indexOf(index3) === -1){
+            controlArr.push(index1);
+            controlArr.push(index2);
+            controlArr.push(index3);
+            count++;
+        }
+    }
+}
+return count;
 };
